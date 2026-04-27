c1fea70c140fe1096239704d7703324b1936b786
flow_os <flow_os@users.noreply.github.com>
2026-04-27 23:09:17 +0300

    Debug: Robust main.jsx and App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+console.log('[DEBUG] main.jsx started');
+
+try {
+  const rootElement = document.getElementById('root');
+  if (!rootElement) {
+    console.error('[DEBUG] Root element NOT FOUND!');
+  } else {
+    console.log('[DEBUG] Root element found, mounting React...');
+    ReactDOM.createRoot(rootElement).render(<App />);
+    console.log('[DEBUG] React render called');
+  }
+} catch (e) {
+  console.error('[DEBUG] CRITICAL MOUNT ERROR:', e);
+  document.body.innerHTML = '<div style="color:red; background:white; padding:20px;"><h1>CRITICAL MOUNT ERROR</h1><pre>' + e.toString() + '</pre></div>';
+}
